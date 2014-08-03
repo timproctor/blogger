d68827f510d96ece527312b7b6e18b5394fae074
timproctor <timandcamiproctor@gmail.com>
2014-08-03 15:00:01 -0600

    added tagging functionality

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,20 @@
+class TagsController < ApplicationController
+
+	def index
+		@tags = Tag.all
+	end
+
+	def show
+		@tag = Tag.find(params[:id])
+	end
+
+	def destroy
+		@tag = Tag.find(params[:id])
+		@tag.destroy
+
+		flash.notice = "Article '#{@tag.name}' Was Deleted!"
+
+		redirect_to tags_path
+		
+	end
+end
